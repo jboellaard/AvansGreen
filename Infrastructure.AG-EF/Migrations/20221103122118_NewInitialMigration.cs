524a894d172ab3fc0a2f082b664403df2437cf0b
2182556 <je.boellaard@student.avans.nl>
2022-11-03 15:38:27 +0100

    Made dbseed for avans green db

---
+++ Infrastructure.AG-EF/Migrations/20221103122118_NewInitialMigration.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.AG_EF.Migrations
+{
+    public partial class NewInitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Canteens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HasWarmMeals = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Canteens", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MealTypes",
+                columns: table => new
+                {
+                    MealTypeId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MealTypes", x => x.MealTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductImages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImageData = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    ImageFormat = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductImages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmailAddress = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    StudentNr = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CityOfSchool = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNr = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumberOfTimesNotCollected = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CanteenEmployees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeeNr = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CanteenId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CanteenEmployees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CanteenEmployees_Canteens_CanteenId",
+                        column: x => x.CanteenId,
+                        principalTable: "Canteens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsAlcoholic = table.Column<bool>(type: "bit", nullable: false),
+                    ProductImageId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_ProductImages_ProductImageId",
+                        column: x => x.ProductImageId,
+                        principalTable: "ProductImages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Packets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PickUpTimeStart = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PickUpTimeEnd = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TimeOfPickUpByStudent = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsAlcoholic = table.Column<bool>(type: "bit", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(6,2)", precision: 6, scale: 2, nullable: false),
+                    CanteenId = table.Column<int>(type: "int", nullable: false),
+                    MealTypeId = table.Column<int>(type: "int", nullable: false),
+                    StudentId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Packets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Packets_Canteens_CanteenId",
+                        column: x => x.CanteenId,
+                        principalTable: "Canteens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Packets_MealTypes_MealTypeId",
+                        column: x => x.MealTypeId,
+                        principalTable: "MealTypes",
+                        principalColumn: "MealTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Packets_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductsInPacket",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PacketId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductsInPacket", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductsInPacket_Packets_PacketId",
+                        column: x => x.PacketId,
+                        principalTable: "Packets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductsInPacket_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "MealTypes",
+                columns: new[] { "MealTypeId", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Bread" },
+                    { 2, "WarmMeal" },
+                    { 3, "Drink" },
+                    { 4, "Snack" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CanteenEmployees_CanteenId",
+                table: "CanteenEmployees",
+                column: "CanteenId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CanteenEmployees_EmployeeNr",
+                table: "CanteenEmployees",
+                column: "EmployeeNr",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Packets_CanteenId",
+                table: "Packets",
+                column: "CanteenId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Packets_MealTypeId",
+                table: "Packets",
+                column: "MealTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Packets_StudentId",
+                table: "Packets",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductImageId",
+                table: "Products",
+                column: "ProductImageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductsInPacket_PacketId",
+                table: "ProductsInPacket",
+                column: "PacketId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductsInPacket_ProductId",
+                table: "ProductsInPacket",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_EmailAddress",
+                table: "Students",
+                column: "EmailAddress",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_StudentNr",
+                table: "Students",
+                column: "StudentNr",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CanteenEmployees");
+
+            migrationBuilder.DropTable(
+                name: "ProductsInPacket");
+
+            migrationBuilder.DropTable(
+                name: "Packets");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Canteens");
+
+            migrationBuilder.DropTable(
+                name: "MealTypes");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "ProductImages");
+        }
+    }
+}
